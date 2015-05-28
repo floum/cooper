c8d36155dd51b2eb239a03dc1477a52b7095060e
Efflam Castel <efflamm.castel@gmail.com>
2015-05-28 21:15:57 +0200

    rm feature

--- spec/features/item_revisioning_spec.rb
+++ spec/features/item_revisioning_spec.rb
@@ -1,14 +1,26 @@
 RSpec.describe 'Item revisioning', type: :aruba do
-  it 'tracks the life of a single item' do
+  before :all do
+    `rm ~/.cooper_item`
     run_simple 'cooper update name:cooper version:0.0.0'
-    run_simple 'cooper update author:unknown location:unknown'
-    run_simple 'cooper update author:floum version:0.0.1'
+    run_simple 'cooper update author:floum location:unknown'
+    run_simple 'cooper rm location'
+    run_simple 'cooper update version:0.0.1'
+  end
+  it 'tracks the life of a single item' do
     run_simple 'cooper read'
     assert_partial_output(
       '{"name":"cooper",
         "version":"0.0.1",
-        "author":"floum",
-        "location":"unknown"}'.gsub(/\n\s+/, ''),
+        "author":"floum"}'.gsub(/\n\s+/, ''),
       output_from('cooper read'))
+  end
+
+  it 'ignores the deleted items' do
+    run_simple 'cooper read --at 2'
+    assert_partial_output(
+      '{"name":"cooper",
+        "version":"0.0.0",
+        "author":"floum"}'.gsub(/\n\s+/, ''),
+      output_from('cooper read --at 2'))
   end
 end
