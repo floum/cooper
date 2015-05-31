7629c8cd2ee494f9b83d76e6e0541b01ab1bd038
Efflam Castel <efflamm.castel@gmail.com>
2015-06-01 00:28:38 +0200

    required cooper in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
+require 'cooper'
+
 Dir.glob(::File.expand_path('../support/*.rb', __FILE__)).each do |f|
   require_relative f
 end
