1df5d98db9dbde02c9258a1c81e8fb4926bb5f0e
efflamm castel <efflamm.castel@gmail.com>
2016-05-25 19:21:57 +0000

    revision source made better

--- lib/cooper/revision_source.rb
+++ lib/cooper/revision_source.rb
@@ -2,7 +2,8 @@ module Cooper
   # Default revisions factory for cooper documents
   class RevisionSource
     def new_revision(object)
-      object.revision_fields
+      object.class
+            .revision_fields
             .each_with_object(OpenStruct.new) do |field, revision|
               revision.send("#{field}=", object.send(field))
             end
--- spec/revision_source_spec.rb
+++ spec/revision_source_spec.rb
@@ -2,10 +2,10 @@ describe Cooper::RevisionSource do
   let(:revision_source) { Cooper::RevisionSource.new }
 
   describe '#new_revision' do
-    let(:object) { double }
+    let(:object) { Object.new }
 
     it 'keeps track of the object revisioned fields' do
-      allow(object).to receive(:revision_fields)
+      allow(object.class).to receive(:revision_fields)
         .and_return([:field_1, :field_2])
       allow(object).to receive(:field_1).and_return(1)
       allow(object).to receive(:field_2).and_return(2)
